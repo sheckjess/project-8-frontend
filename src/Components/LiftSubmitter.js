f17a55e2e48f40b8154d431ad1a821a69981787d
Matthew Lewis <matthew.knight.lewis@gmail.com>
2020-11-12 12:15:06 -0500

    FEATURE: user can submit a lift in the /lifting route with a variable amount of sets also  with variable reps and pounds.

--- src/Components/LiftSubmitter.js
+++ src/Components/LiftSubmitter.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Calendar from "react-calendar";
-const axios = require('axios')
+// eslint-disable-next-line
+const axios = require("axios");
 
 class LiftSubmitter extends Component {
   constructor(props) {
@@ -11,46 +12,91 @@ class LiftSubmitter extends Component {
       musclegroup: "",
       lift: "",
       sets: [],
+      _numberOfSets: 0,
     };
   }
 
   onChangeDate = (e) => {
-    console.log(e)
-    this.setState({date: e.target.value});
+    this.setState({ date: e });
   };
 
   onChangeMuscleGroup = (e) => {
-    console.log(e)
-    this.setState({musclegroup: e.target.value});
+    this.setState({ musclegroup: e.target.value });
   };
 
   onChangeLiftName = (e) => {
-    console.log(e)
-    this.setState({lift: e.target.value});
+    this.setState({ lift: e.target.value });
   };
 
   onChangeSets = (e) => {
-    console.log(e)
-    //For each number in 'Sets", there should be a 'Reps' Rendered in the form
+    this.setState({ _numberOfSets: e.target.value });
+  };
+
+  onChangeReps = (e) => {
+    console.log(e);
+    //makes a copy of the sets array in state
+    var tempArray = this.state.sets
+
+    //if the item at the id already exists
+    if (tempArray[e.target.id]) {
+      tempArray[e.target.id].reps = e.target.value
+    }
+    else {
+      var newSet = {reps: 0, pounds: 777}
+      newSet.reps = e.target.value
+      tempArray[e.target.id] = newSet
+    }
+
+    this.setState({sets: tempArray})
+  };
+
+  onChangeRepWeight = (e) => {
+    console.log(e);
+    //makes a copy of the sets array in state
+    var tempArray = this.state.sets
+
+    //if the item at the id already exists
+    if (tempArray[e.target.id]) {
+      tempArray[e.target.id].pounds = e.target.value
+    }
+    else {
+      var newSet = {reps: 777, pounds: 0}
+      newSet.pounds = e.target.value
+      tempArray[e.target.id] = newSet
+    }
+
+    this.setState({sets: tempArray})
   };
 
   onSubmit = (e) => {
     e.preventDefault();
-    //var tempLift = {}
+    
+    console.log(this.state)
+
     //axios.post('https://sculpt-fitness.herokuapp.com/lifting/add', tempLift)
   };
 
   render() {
+    //create a reps element for each number in numberOfSets
+    var arrayOfReps = [];
+    for (let i = 0; i < this.state._numberOfSets; i++) {
+      arrayOfReps.push(
+        <div>
+          <label>Set {i+1} Reps: </label>
+          <input id={i} required onChange={this.onChangeReps} type="number" placeholder="5" min='1' max='100'></input>
+          <label>Set {i+1} Weight: </label>
+          <input id={i} required onChange={this.onChangeRepWeight} type="number" placeholder="20" min='10' step='5' max='1000'></input>
+        </div>
+      );
+    }
+
     return (
       <>
         <h3>Add Lift</h3>
         <form onSubmit={this.onSubmit}>
           <div className="form-group">
             <label>Date: </label>
-            <Calendar
-              defaultView="month"
-              onChange={this.onChangeDate}
-            />
+            <Calendar defaultView="month" onChange={this.onChangeDate} />
           </div>
           <div className="form-group">
             <label>Muscle Group: </label>
@@ -82,6 +128,9 @@ class LiftSubmitter extends Component {
               max="10"
             ></input>
           </div>
+
+          {arrayOfReps}
+
           <button type="submit" className="">
             Submit
           </button>
