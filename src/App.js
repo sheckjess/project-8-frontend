dc94f03752c0296e93e03e66bc9f597cd252cefd
Jesse Maretz <jesse.maretz@gmail.com>
2020-11-12 09:59:39 -0500

    using react router now

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, {Component} from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { render } from '@testing-library/react';
 import HomePage from './components/HomePage'
 
@@ -11,7 +12,10 @@ class App extends Component {
   render(){
     return(
       <div>
-        <HomePage />
+        <Router>
+          <Route path="/"
+          component={HomePage} />
+        </Router>
       </div>
     );
   }
