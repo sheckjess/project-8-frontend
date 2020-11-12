1fc6db66f9b37fbae24812a8bfce7c37e214d336
Matthew Lewis <matthew.knight.lewis@gmail.com>
2020-11-12 09:57:02 -0500

    added a new component called LiftSubmitter, it's not functional yet. Once it is plugged in to the Links and Routes I'll continue working on it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, {Component} from 'react'
 import { render } from '@testing-library/react';
-import LandButtons from './components/LandButtons'
+import LandButtons from './Components/LandButtons'
 
 class App extends Component {
   constructor(props){
