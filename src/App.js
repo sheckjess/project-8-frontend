ee5a2d0ae995a8845c10130436d6c9b0f9a3cbf7
Jesse Maretz <jesse.maretz@gmail.com>
2020-11-12 13:55:52 -0500

    idk what the hell happened but we fixed it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import React, {Component} from 'react'
 import { render } from '@testing-library/react';
-import HomePage from './Components/HomePage'
-import NavBar from './Components/NavBar'
+import HomePage from './components/HomePage'
+import NavBar from './components/NavBar'
+import CardioSubmitter from './components/CardioSubmitter'
+import LiftSubmitter from './components/LiftSubmitter'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 
 class App extends Component {
@@ -13,8 +16,16 @@ class App extends Component {
   render(){
     return(
       <div>
+        <NavBar />
+        <Router>
+          <Route exact path="/"
+          component={HomePage} />
+          <Route exact path="/cardio"
+          component={CardioSubmitter} />
+          <Route exact path="/lifting"
+          component={LiftSubmitter} />
+        </Router>
         
-        <HomePage />
       </div>
     );
   }
